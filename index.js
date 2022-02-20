92128432f09b3cd60c922117278d43379762faf8
Naz <nazeefsuleman@gmail.com>
2022-02-20 11:52:19 -0500

    Made onclick event for tipButtons with GetClassName, still need to make buttons change colors when another button is clicked

--- index.js
+++ index.js
@@ -1,21 +1,37 @@
 //<------- Variables ------->
 let billAmountInput = document.getElementById("billAmountInput");
 let nbPeopleInput = document.getElementById("nbofPeopleInput");
+let tipButtons = document.getElementsByClassName("tip-button");
+
+
+
 //<------- Event Listeners ------->
 billAmountInput.addEventListener("click", changeColor);
-billAmountInput.addEventListener("focusout",verifyInput);
-nbPeopleInput.addEventListener("click",changeColor);
-nbPeopleInput.addEventListener("focusout",verifyInput);
+billAmountInput.addEventListener("focusout", verifyInput);
+nbPeopleInput.addEventListener("click", changeColor);
+nbPeopleInput.addEventListener("focusout", verifyInput);
+
+Array.from(tipButtons).forEach(function (tipButton) {
+    tipButton.addEventListener("click", changeBtnColor);
+
+    function changeBtnColor() {
+        this.style.backgroundColor = "hsl(172, 67%, 45%)";
+        this.style.color = "hsl(183, 100%, 15%)";
+    }
+})
+
 //<------- Functions ------->
 function changeColor() {
-    this.style.outlineColor = "hsl(172, 67%, 45%)";
-    
+    if (this == billAmountInput || this == nbPeopleInput) {
+        this.style.outlineColor = "hsl(172, 67%, 45%)";
+    }
+
 }
 
-function verifyInput(){
-    if(billAmountInput.value == "0" || billAmountInput.value == ""){
+function verifyInput() {
+    if (this.value == "0" || this.value == "") {
         this.style.border = "1px solid red";
-    }else{
+    } else {
         this.style.border = "none";
     }
 }
