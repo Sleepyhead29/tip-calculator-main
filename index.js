3397d144f8a021b72c55c4ebd34cf47f8653a110
Naz <nazeefsuleman@gmail.com>
2022-02-20 09:28:11 -0500

    made the Onfocus border change for the input fields

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 //<------- Variables ------->
+let billAmountInput = document.getElementById("billAmountInput");
+let nbPeopleInput = document.getElementById("nbofPeopleInput");
 //<------- Event Listeners ------->
+billAmountInput.addEventListener("click", changeColor);
+billAmountInput.addEventListener("focusout",verifyInput);
+nbPeopleInput.addEventListener("click",changeColor);
+nbPeopleInput.addEventListener("focusout",verifyInput);
 //<------- Functions ------->
+function changeColor() {
+    this.style.outlineColor = "hsl(172, 67%, 45%)";
+    
+}
+
+function verifyInput(){
+    if(billAmountInput.value == "0" || billAmountInput.value == ""){
+        this.style.border = "1px solid red";
+    }else{
+        this.style.border = "none";
+    }
+}
